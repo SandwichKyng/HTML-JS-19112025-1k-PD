560f108c9b36bdc9ae841fe6c5c0f1a1f5561278
dominiks <dominiksvecbastiks@gmail.com>
2025-11-19 15:48:06 +0200

    kategorijas izveidotas

--- app.js
+++ app.js
@@ -4,27 +4,29 @@ const ķermeņaGarums = document.getElementById("garums");
 const ķermeņaSvars = document.getElementById("svars");
 const calcButton = document.getElementById("calc");
 const rezultātaAprēķins = document.getElementById("output");
+const rezultātaAprēķins2 = document.getElementById("output2");
 
 calcButton.addEventListener('click', handleClick);
 
 function handleClick() {
 
-const NeĶM = 18.5
-const NoĶM = 18.5
-const LĶM = 25 
-const A = 30
-
 const garums = ķermeņaGarums.value;
 const svars = ķermeņaSvars.value;
 
-rezultātaAprēķins.innerHTML = svars/garums
-
-
-}
+rezultātaAprēķins.innerHTML = svars/(garums/100);
 
 if(rezultātaAprēķins <= 18,5) {
-    
-   rezultātaAprēķins.innerHTML = "Nepietiekama ķermeņa masa"
-} else {
-    rezultātaAprēķins.innerHTML = "Pietiekama ķermeņa masa"
+   rezultātaAprēķins2.innerHTML = "Nepietiekama ķermeņa masa"
+} 
+if(rezultātaAprēķins >= 18,5) {
+   rezultātaAprēķins2.innerHTML = "Normāla ķermeņa masa"
 }
+if(rezultātaAprēķins >= 25) {
+   rezultātaAprēķins2.innerHTML = "Lieka ķermeņa masa"
+}
+if(rezultātaAprēķins >= 30) {
+   rezultātaAprēķins2.innerHTML = "Aptaukošanās"
+}
+
+}
+
