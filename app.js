5df66b764107a6a0d000cc4f26e8afda3f9b948b
dominiks <dominiksvecbastiks@gmail.com>
2025-11-19 15:21:09 +0200

    working calc

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-// your code goes here
+
+
+const ķermeņaGarums = document.getElementById("garums");
+const ķermeņaSvars = document.getElementById("svars");
+const calcButton = document.getElementById("calc");
+const rezultātaAprēķins = document.getElementById("output");
+
+calcButton.addEventListener('click', handleClick);
+
+function handleClick() {
+
+const NeĶM = 18.5
+const NoĶM = 18.5
+const LĶM = 25 
+const A = 30
+
+const garums = ķermeņaGarums.value;
+const svars = ķermeņaSvars.value;
+
+rezultātaAprēķins.innerHTML = svars/garums
+
+
+}
+
+if(rezultātaAprēķins <= 18,5) {
+    
+   rezultātaAprēķins.innerHTML = "Nepietiekama ķermeņa masa"
+} else {
+    rezultātaAprēķins.innerHTML = "Pietiekama ķermeņa masa"
+}
